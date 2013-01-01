bc602bb68136f603ce2719279543babf8096a737
Arshad <arshad@chummun.com>
2013-01-02 00:31:48 +0400

    make sure menu is enabled before rendering

--- assets/javascripts/radix-admin.js
+++ assets/javascripts/radix-admin.js
@@ -2,10 +2,10 @@
 Drupal.behaviors.radix_admin = {
   attach: function (context, settings) {
     $('#radix-admin-menu > ul.menu > li', context).mouseenter(function () {
-      $(this).addClass('over').find('ul.menu').show();
+      $(this).addClass('over');
     });
     $('#radix-admin-menu > ul.menu > li', context).mouseleave(function () {
-      $(this).removeClass('over').find('ul.menu').hide();
+      $(this).removeClass('over');
     });
   }
 };
